d25dd0c1a7396500f5af99bbe24e5384a2c1fa25
NavaraImp <86377780+NavaraImp@users.noreply.github.com>
2021-06-24 02:31:07 +0530

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,9 +1,14 @@
   
  <script>
+  function a()
+ {
    var pic= [
     
        "https://1.bp.blogspot.com/-fEcaqayHiH0/YI_gHOzKjcI/AAAAAAAAARg/yvVdfIWP4T4uJ03U1p-vHjsGCYRzAa8AACLcBGAsYHQ/s16000/2.png"
      , "https://1.bp.blogspot.com/-wrvx2JEI-LY/YI_ZMxryVwI/AAAAAAAAARM/KYLqcuHgW8sQefVvxCM2zSsf1CjKl7kGACLcBGAsYHQ/s1518/1.jpg"
     , "https://1.bp.blogspot.com/-Yv8JIaTCqr8/YJE9wDg-m5I/AAAAAAAAASw/RuhXI7Lq0iMaTa8T9IP-0ImfBZKU2xdsQCLcBGAsYHQ/s0/004.jpg"  ] ;
+ 
+ return pic;
+ }
   
   </script>
